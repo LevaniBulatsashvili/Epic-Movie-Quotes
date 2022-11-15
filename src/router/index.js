ea06254b0bed71a714be3f5f9dbc657388412c07
Levani Bulatsashvili <levan.bulatsashvili@gmail.com>
2022-11-15 15:37:42 +0400

    initial-setup

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../views/HomeView.vue"),
+    },
+    {
+      path: "/registration",
+      name: "registration",
+      component: () => import("../views/RegistrationView.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
+    },
+    {
+      path: "/news-feed",
+      name: "newsFeed",
+      component: () => import("../views/NewsFeedView.vue"),
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: () => import("../views/ProfileView.vue"),
+    },
+    {
+      path: "/movies",
+      name: "movies",
+      component: () => import("../views/MoviesView.vue"),
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "notFound",
+      component: () => import("../views/NotFoundView.vue"),
+    }
+  ],
+});
+
+export default router;
