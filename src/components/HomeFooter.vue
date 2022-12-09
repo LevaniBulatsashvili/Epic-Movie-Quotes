430e45cdf1362a5d50a2b5b6e4a9e6d592c5db6c
Levani Bulatsashvili <levan.bulatsashvili@gmail.com>
2022-12-09 09:52:45 +0400

    ref: refactor-code

--- src/components/HomeFooter.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <footer>
-    <div
-      class="font-[Halvetica Neue] my-[1.5rem] mx-[7rem] text-[1.2rem] font-medium text-[#DDCCAA]"
-    >
-      © 2022 movie quotes. All rights reserved.
-    </div>
-  </footer>
-</template>
